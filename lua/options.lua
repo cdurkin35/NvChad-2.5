84e26d9da10c3a293e5ec2e5f015d00d84c68f7a
Charles Durkin <cdurkin35@gmail.com>
2025-03-27 17:41:28 -0400

    Add custom snippets and various other small configuration changes

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,12 @@
-require "nvchad.options"
+require("nvchad.options")
 
 local o = vim.o
+local g = vim.g
 
 o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
 
+g.vscode_snippets_path = "~/.config/nvim/lua/snippets"
+-- o.cursorlineopt ='both' -- to enable cursorline!
